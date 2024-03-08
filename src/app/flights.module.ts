25154f1df4302d6c3bb175ca53ab10b2ef8537db
AnoraDragoon <anoradragoon@gmail.com>
2024-03-08 18:52:19 +0100

    Dynamic Module Federation with Angular

--- src/app/flights.module.ts
+++ src/app/flights.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { HomeComponent } from './web/home/home.component';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './routes';
-
+import { FormPageComponent } from './web/form-page/form-page.component';
 
 
 @NgModule({
-    declarations: [HomeComponent],
-    imports: [
-        CommonModule,
-        RouterModule.forChild(APP_ROUTES)
-    ]
+    declarations: [HomeComponent, FormPageComponent],
+    imports: [CommonModule, RouterModule.forChild(APP_ROUTES)],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class FlightsModule { }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./web/home/home.component";
+import { FormPageComponent } from "./web/form-page/form-page.component";
 
 export const APP_ROUTES: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' }
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'form', component: FormPageComponent },
 ];
